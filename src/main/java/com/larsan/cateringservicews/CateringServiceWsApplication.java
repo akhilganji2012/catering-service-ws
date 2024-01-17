1773d8380911389300232cb097d063a3976f07b3
Akhil Ganji <akhilganji@Akhils-MBP.attlocal.net>
2024-01-17 10:48:38 -0600

    intial push

---
+++ src/main/java/com/larsan/cateringservicews/CateringServiceWsApplication.java
@@ -0,0 +1,13 @@
+package com.larsan.cateringservicews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringServiceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringServiceWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larsan/cateringservicews/CateringServiceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larsan.cateringservicews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringServiceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
